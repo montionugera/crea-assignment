3c5159ff5a984d148aebc985bfb39dee2afab0d4
Pasit Nusso <montionugera@gmail.com>
2021-09-01 16:02:14 +0700

    Remove print Change Shadow variable

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ class TripGraph:
     origin: str
     _routes_by_destination: Dict[str, List[Route]]
     _not_added_routes: List[Route]
-    _routes_by_origin: Dict[str, Route]
+    _routes_by_origin: Dict[str, List[Route]]
 
-    def __init__(self, origin: str, routes: List[Route]):
-        self.origin = origin
+    def __init__(self, _origin: str, routes: List[Route]):
+        self.origin = _origin
         self._not_added_routes = [route for route in routes]
         self._not_added_routes.sort(key=lambda x: x.minutes_spend)
         self.cost_by_origin_destination_pair = {}
@@ -30,9 +30,9 @@ class TripGraph:
         for route in self._not_added_routes:
             self._routes_by_origin[route.origin].append(route)
 
-        origin_routes = self._routes_by_origin[origin]
+        origin_routes = self._routes_by_origin[_origin]
         if len(origin_routes) == 0:
-            Exception(f"Origin '{origin}' not found in routes")
+            Exception(f"Origin '{_origin}' not found in routes")
 
         self._routes_by_destination = {}
         shortest_origin_route = origin_routes[0]
@@ -48,12 +48,11 @@ class TripGraph:
         routes.append(route)
         self._routes_by_destination[route.destination] = routes
 
-    def find_shortest_path(self, destination: str) -> List[Route]:
-        if destination == self.origin:
+    def find_shortest_path(self, _destination: str) -> List[Route]:
+        if _destination == self.origin:
             Exception(f"Invalid input destination == origin")
-        match_routes = self._routes_by_destination.get(destination, None)
+        match_routes = self._routes_by_destination.get(_destination, None)
         while match_routes is None:
-            print(f"self._routes_by_destination {self._routes_by_destination}")
             edges = [self.origin] + [node_key for node_key in list(self._routes_by_destination.keys())]
             cost_by_edge = {}
             for edge in edges:
@@ -66,13 +65,12 @@ class TripGraph:
                 if _route.origin in edges:
                     _forecast_cost = _route.minutes_spend + cost_by_edge.get(_route.origin)
                     if target_route_cost is None or target_route_cost > _forecast_cost:
-                        print(f"target_route {target_route}")
                         target_route_cost, target_route = _forecast_cost, _route
             if target_route is None:
                 raise RouteNotFoundException("destination can not reach")
             else:
                 self._add_route(target_route)
-                match_routes = self._routes_by_destination.get(destination, None)
+                match_routes = self._routes_by_destination.get(_destination, None)
         return match_routes
 
 
@@ -89,8 +87,8 @@ def routes_from_csv(file_path: str) -> List[Route]:
     with open(file_path, newline='') as csv_file:
         data = list(csv.reader(csv_file))
         rows = []
-        for origin, destination, minutes_spend in data:
-            rows.append(Route(origin=origin, destination=destination, minutes_spend=minutes_spend))
+        for _origin, _destination, minutes_spend in data:
+            rows.append(Route(origin=_origin, destination=_destination, minutes_spend=minutes_spend))
         return rows
 
 
@@ -112,15 +110,15 @@ if __name__ == '__main__':
     if input_file == '':
         print(HELP_MSG)
         sys.exit(2)
-    routes = routes_from_csv(input_file)
+    routes_from_file = routes_from_csv(input_file)
     origin = input("What station are you getting on the train?:")
-    graph = TripGraph(origin, routes)
+    graph = TripGraph(origin, routes_from_file)
     destination = input("What station are you getting off the train?:")
     try:
-        routes = graph.find_shortest_path(destination=destination)
-        stop_num = len(routes) - 1
-        total_time_in_minutes = sum([route.minutes_spend for route in routes])
-        print(f'Your trip from {origin} to {destination} include {stop_num} stops' + \
+        routes_to_destination = graph.find_shortest_path(_destination=destination)
+        stop_num = len(routes_to_destination) - 1
+        total_time_in_minutes = sum([route.minutes_spend for route in routes_to_destination])
+        print(f'Your trip from {origin} to {destination} include {stop_num} stops' +
               f'and will take {total_time_in_minutes} minutes.')
     except RouteNotFoundException:
         print(f"Your trip from {origin} to {destination} can not be found.")
